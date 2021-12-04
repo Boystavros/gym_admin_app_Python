58d6efb8749340784413a4ab207ff36fcb8c7df9
Sacha Ponniah <sponniah812@gmail.com>
2021-12-04 17:41:41 +0000

    Adds save() functionality to fit_classes repository, controller and template.

--- controllers/fit_class_controller.py
+++ controllers/fit_class_controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+from flask.helpers import url_for
 from models.fit_class import Fit_class
 import repositories.fit_class_repository as fit_class_repository
 
@@ -10,6 +11,15 @@ def fit_classes():
     fit_classes = fit_class_repository.select_all()
     return render_template("/classes/index.html", fit_classes=fit_classes)
 
-@fit_classes_blueprint.route("/fit_classes/add_fit_class")
-def add_fit_class():
-    return render_template("/classes/add_fit_class.html")
+@fit_classes_blueprint.route("/classes/add", methods=['POST'])
+def add_class():
+    name = request.form['name']
+    category = request.form['category']
+    instructor = request.form['instructor']
+    date = request.form['date']
+    time = request.form['time']
+    location = request.form['location']
+    fit_class = Fit_class(name, category, instructor, date, time, location)
+    fit_class_repository.save(fit_class)
+    return redirect(url_for('.fit_classes'))
+
